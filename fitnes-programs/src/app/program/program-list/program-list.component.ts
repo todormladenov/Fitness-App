0211a28711920a8c655d9d2a70d0048150382f3e
Todor Mladenov <toshko958@gmail.com>
2024-03-21 08:34:31 +0100

    Create program components
    program list component
    create program component
    details component

---
+++ fitnes-programs/src/app/program/program-list/program-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-program-list',
+  templateUrl: './program-list.component.html',
+  styleUrls: ['./program-list.component.css']
+})
+export class ProgramListComponent {
+
+}
--- fitnes-programs/src/app/program/program.module.ts
+++ fitnes-programs/src/app/program/program.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateProgramComponent } from './create-program/create-program.component';
+import { ProgramListComponent } from './program-list/program-list.component';
+import { DetailsComponent } from './details/details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateProgramComponent,
+    ProgramListComponent,
+    DetailsComponent
+  ],
   imports: [
     CommonModule
   ]
