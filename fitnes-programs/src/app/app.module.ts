d30d46d6b03c452a7c5032350508571ab96a21f6
Todor Mladenov <toshko958@gmail.com>
2024-03-21 08:30:10 +0100

    Created program module

--- fitnes-programs/src/app/app.module.ts
+++ fitnes-programs/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
+import { ProgramModule } from './program/program.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    UserModule
+    UserModule,
+    ProgramModule
   ],
   providers: [],
   bootstrap: [AppComponent]
