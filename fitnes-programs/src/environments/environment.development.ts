ce3452b3a92e3c57467552786f26bf10013edb9e
Todor Mladenov <toshko958@gmail.com>
2024-03-22 12:46:07 +0100

    Created environment files

---
+++ fitnes-programs/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    api: 'https://parseapi.back4app.com/',
+    apiHeaders: {
+        'X-Parse-Application-Id': 'eAf5TieNFDrtEOzQRWpiN3HxoE4uvot35RktZo2h',
+        'X-Parse-REST-API-Key': 'QGCM4xVD8u3SYHl43KGjwBd9is2vDDEW0GH4PVQ7',
+    }
+};
