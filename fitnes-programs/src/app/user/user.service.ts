95b0e679819d4df00ff57d8b341131f5ee393b49
Todor Mladenov <toshko958@gmail.com>
2024-03-23 11:03:14 +0100

    Update UserService
    - Added logout functionality.
    - Added session token for user authentication and storage management.
    - Updated user state upon successful login and clearing it upon logout.
    - Added isLoggedIn property to check the authentication status of the user.

--- fitnes-programs/src/app/user/user.service.ts
+++ fitnes-programs/src/app/user/user.service.ts
@@ -1,19 +1,50 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserForAuth } from './types/user';
+import { tap } from 'rxjs';
+import { removeItem, setItem } from './utils/storage-management';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: UserForAuth | undefined;
+
+  get isLoggedIn() {
+    return !!this.user;
+  }
 
   constructor(private http: HttpClient) { }
 
   registerUser(email: string, username: string, password: string) {
-    return this.http.post<UserForAuth>('api/users', { email, username, password });
+    return this.http
+      .post<UserForAuth>('api/users', { email, username, password })
+      .pipe(tap((user) => {
+        setItem('AuthToken', user?.sessionToken);
+      }));
   }
 
   loginUser(username: string, password: string) {
-    return this.http.post<UserForAuth>('api/login', { username, password })
+    return this.http
+      .post<UserForAuth>('api/login', { username, password })
+      .pipe(tap((user) => {
+        setItem('AuthToken', user?.sessionToken);
+        this.user = user;
+      }))
+  }
+
+  logoutUser() {
+    return this.http
+      .post('api/logout', {})
+      .pipe(tap(() => {
+        removeItem('AuthToken');
+        this.user = undefined;
+      }))
+  }
+
+  getCurrentUser() {
+    return this.http
+      .get('api/users/me')
+      
   }
 }
