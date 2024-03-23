5179b713602e7ec37928106ff3fc4ade8984390e
Todor Mladenov <toshko958@gmail.com>
2024-03-23 10:51:35 +0100

    Add session token to headers

--- fitnes-programs/src/app/app.interceptor.ts
+++ fitnes-programs/src/app/app.interceptor.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor
 import { Injectable, Provider } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment.development";
+import { getItem } from "./user/utils/storage-management";
 
 @Injectable()
 
@@ -10,13 +11,19 @@ class AppInterceptor implements HttpInterceptor {
     private headers = new HttpHeaders(environment.apiHeaders);
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const sessionToken = getItem('AuthToken');
+
+        if (sessionToken) {
+            this.headers.append('X-Parse-Session-Token', sessionToken);    
+        }
+
         if (req.url.startsWith('api/')) {
             req = req.clone({
                 url: req.url.replace('api/', this.api),
-                headers: this.headers,
+                headers: this.headers
             });
         }
-
+        
         return next.handle(req)
     }
 }
