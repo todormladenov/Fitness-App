816db22f41b7ec6ca26df9a7536a8fe599367ad7
Todor Mladenov <toshko958@gmail.com>
2024-03-23 11:05:57 +0100

    Updated headers to dynamically display navigation

--- fitnes-programs/src/app/core/header/header.component.ts
+++ fitnes-programs/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private userService: UserService, private router: Router) { }
+
+  get isLogged(){
+    return this.userService.isLoggedIn
+  }
+
+  logout(){
+    this.userService.logoutUser().subscribe(() => {
+      this.router.navigate(['/user/login'])
+    })
+  }
 }
