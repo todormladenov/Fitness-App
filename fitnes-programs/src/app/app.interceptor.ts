956d2df7e12dc3e05b34b245da555109936ba2fd
Todor Mladenov <toshko958@gmail.com>
2024-03-23 18:39:44 +0100

    Updated interceptor to add session token to headers

--- fitnes-programs/src/app/app.interceptor.ts
+++ fitnes-programs/src/app/app.interceptor.ts
@@ -1,21 +1,17 @@
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable, Provider } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, catchError } from "rxjs";
 import { environment } from "src/environments/environment.development";
 import { getItem } from "./user/utils/storage-management";
+import { Router } from "@angular/router";
 
 @Injectable()
 
 class AppInterceptor implements HttpInterceptor {
     private api = environment.api;
     private headers = new HttpHeaders(environment.apiHeaders);
-
+    constructor(private router: Router) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const sessionToken = getItem('AuthToken');
-
-        if (sessionToken) {
-            this.headers.append('X-Parse-Session-Token', sessionToken);    
-        }
 
         if (req.url.startsWith('api/')) {
             req = req.clone({
@@ -23,8 +19,25 @@ class AppInterceptor implements HttpInterceptor {
                 headers: this.headers
             });
         }
+
+        const sessionToken = getItem('AuthToken');
         
-        return next.handle(req)
+        if (sessionToken) {
+
+            req = req.clone({
+                headers: req.headers.set('X-Parse-Session-Token', sessionToken)
+
+            })
+        }
+
+        return next.handle(req).pipe(
+            catchError((err) => {
+                if (err.error.code === 209) {
+                    this.router.navigate(['/user/login'])
+                }
+                return [err]
+            })
+        )
     }
 }
 
