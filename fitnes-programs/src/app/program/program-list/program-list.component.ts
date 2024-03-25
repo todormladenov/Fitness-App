799ebc9790ac9e7bb700dbc6e0b5237699cc9a3d
Todor Mladenov <toshko958@gmail.com>
2024-03-25 17:15:26 +0100

    Updated program list to show and hide global loader

--- fitnes-programs/src/app/program/program-list/program-list.component.ts
+++ fitnes-programs/src/app/program/program-list/program-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgramService } from '../program.service';
 import { Program } from '../types/program';
+import { GlobalLoaderService } from 'src/app/core/global-loader.service';
 
 @Component({
   selector: 'app-program-list',
@@ -10,15 +11,18 @@ import { Program } from '../types/program';
 export class ProgramListComponent implements OnInit {
   programsList: Program[] = [];
 
-  constructor(private programService: ProgramService) { }
+  constructor(private programService: ProgramService, private globalLoaderService: GlobalLoaderService) { }
 
   ngOnInit(): void {
     this.getPrograms();
   }
 
   getPrograms() {
+    this.globalLoaderService.isLoading = true;
+
     this.programService.getPrograms().subscribe(data => {
-      this.programsList = data.results;      
+      this.programsList = data.results;
+      this.globalLoaderService.isLoading = false;
     })
   }
 }
