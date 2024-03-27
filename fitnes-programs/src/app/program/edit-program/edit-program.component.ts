1cdb28bae90c94a98b7447c0223c03ba4aff63e7
Todor Mladenov <toshko958@gmail.com>
2024-03-27 11:05:32 +0100

    Populated edit form with program data

--- fitnes-programs/src/app/program/edit-program/edit-program.component.ts
+++ fitnes-programs/src/app/program/edit-program/edit-program.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { Program } from '../types/program';
+import { ProgramService } from '../program.service';
+import { ActivatedRoute } from '@angular/router';
+import { GlobalLoaderService } from 'src/app/core/global-loader.service';
 
 @Component({
   selector: 'app-edit-program',
   templateUrl: './edit-program.component.html',
   styleUrls: ['./edit-program.component.css']
 })
-export class EditProgramComponent {
+export class EditProgramComponent implements OnInit {
+  programId: string = '';
+  program = {} as Program;
 
+  form = this.fb.group({
+    title: ['', [Validators.required, Validators.maxLength(25)]],
+    type: ['', [Validators.required]],
+    image: ['', [Validators.required]],
+    price: ['', [Validators.required, Validators.min(0)]],
+    description: ['', [Validators.required, Validators.minLength(20)]],
+  })
+
+  constructor(
+    private programService: ProgramService,
+    private activeRoute: ActivatedRoute,
+    private globalLoaderService: GlobalLoaderService,
+    private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.getProgram();
+  }
+
+  edit() {
+    
+  }
+
+  get globalLoaderIsLoading() {
+    return this.globalLoaderService.isLoading;
+  }
+
+  getProgram() {
+    this.globalLoaderService.showLoader();
+    this.activeRoute.params.subscribe((data) => {
+      this.programId = data['programId'];
+
+      this.programService.getProgramById(this.programId).subscribe((data) => {
+        this.program = data;
+        const { title, type, image, price, description } = data;
+
+        this.form.setValue({
+          title,
+          type,
+          image,
+          price,
+          description
+        });
+
+        this.globalLoaderService.hideLoader();
+      });
+    })
+  }
 }
--- fitnes-programs/src/app/program/program.module.ts
+++ fitnes-programs/src/app/program/program.module.ts
@@ -4,7 +4,7 @@ import { CreateProgramComponent } from './create-program/create-program.componen
 import { ProgramListComponent } from './program-list/program-list.component';
 import { DetailsComponent } from './details/details.component';
 import { ProgramRoutingModule } from './program-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { ProgramComponent } from './program/program.component';
 import { EditProgramComponent } from './edit-program/edit-program.component';
@@ -23,7 +23,8 @@ import { EditProgramComponent } from './edit-program/edit-program.component';
     CommonModule,
     ProgramRoutingModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     ProgramComponent
