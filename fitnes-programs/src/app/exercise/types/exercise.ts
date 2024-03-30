a4990d46b45adda471f4f7ef839487b789f11bc8
Todor Mladenov <toshko958@gmail.com>
2024-03-30 11:55:09 +0100

    Create Exercise Interface

---
+++ fitnes-programs/src/app/exercise/types/exercise.ts
@@ -0,0 +1,12 @@
+import { Pointer } from "src/app/shared/types/pointer"
+
+export interface Exercise {
+    objectId: string | null,
+    createdAt: string,
+    updatedAt: string,
+    title: string,
+    sets: string,
+    repetitions: string,
+    program: Pointer | null,
+    owner: Pointer | null
+}
