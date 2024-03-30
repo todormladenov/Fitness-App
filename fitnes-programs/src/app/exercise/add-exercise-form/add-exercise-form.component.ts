723b0ea2a7b31e691d7d18ca7513126a15c4fa3e
Todor Mladenov <toshko958@gmail.com>
2024-03-30 15:38:40 +0100

    Added save functionality

--- fitnes-programs/src/app/exercise/add-exercise-form/add-exercise-form.component.ts
+++ fitnes-programs/src/app/exercise/add-exercise-form/add-exercise-form.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Exercise } from '../types/exercise';
+import { ExerciseService } from '../exercise.service';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-add-exercise-form',
   templateUrl: './add-exercise-form.component.html',
   styleUrls: ['./add-exercise-form.component.css']
 })
-export class AddExerciseFormComponent {
+export class AddExerciseFormComponent implements OnInit {
   @Input('exercise') exercise = {} as Exercise;
   @Input('deleteExercise') deleteExercise!: (index: number) => void;
   @Input('index') index!: number;
+  programId: string = '';
+  userId: string = '';
 
   isDisabled = true;
+
+  constructor(
+    private exerciseService: ExerciseService,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      this.programId = data['programId'];
+    });
+  }
 
   edit() {
     this.isDisabled = false;
@@ -21,7 +38,24 @@ export class AddExerciseFormComponent {
     this.deleteExercise(this.index);
   }
 
-  cancel(){
+  cancel() {
     this.isDisabled = true;
+  }
+
+  save(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    if (this.userService.userId) {
+      this.userId = this.userService.userId;
+    }
+
+    const { title, sets, repetitions } = form.value;
+
+    this.exerciseService.create(title, sets, repetitions, this.programId, this.userId).subscribe((res) => {
+      this.exercise.objectId = res.objectId;
+      this.isDisabled = true;
+    });
   }
 }
