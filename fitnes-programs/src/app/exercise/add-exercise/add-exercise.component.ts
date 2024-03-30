bc0227342ee26c461622487298c3f823e4c0577f
Todor Mladenov <toshko958@gmail.com>
2024-03-30 16:13:54 +0100

    Added functionality to load all exercises of the program

--- fitnes-programs/src/app/exercise/add-exercise/add-exercise.component.ts
+++ fitnes-programs/src/app/exercise/add-exercise/add-exercise.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Exercise } from '../types/exercise';
 import { ActivatedRoute } from '@angular/router';
+import { ExerciseService } from '../exercise.service';
 
 @Component({
   selector: 'app-add-exercise',
@@ -11,7 +12,7 @@ export class AddExerciseComponent implements OnInit {
   exerciseList: Exercise[] = [];
   programId: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private exerciseServices: ExerciseService, private activatedRoute: ActivatedRoute) { }
 
   addExercise() {
     const newExercise: Exercise = {
@@ -31,8 +32,15 @@ export class AddExerciseComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((data) => {
       this.programId = data['programId'];
+      this.loadExercise();
       this.addExercise();
     });
+  }
+
+  loadExercise() {
+    this.exerciseServices.getExerciseByProgramId(this.programId).subscribe((data) => {
+      this.exerciseList = data.results;
+    })
   }
 
   deleteExercise = (index: number) => {
--- fitnes-programs/src/app/exercise/exercise.service.ts
+++ fitnes-programs/src/app/exercise/exercise.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { createPointer } from '../shared/utils/create-pointer';
 import { CreatedExercise } from './types/exercise';
+import { ReadingExerciseApiResponse } from '../shared/types/response';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,15 @@ import { CreatedExercise } from './types/exercise';
 export class ExerciseService {
 
   constructor(private http: HttpClient) { }
+
+  getExerciseByProgramId(programId: string) {
+    const pointer = {
+      program: createPointer('Program', programId)
+    }
+
+    const query = encodeURIComponent(JSON.stringify(pointer));
+    return this.http.get<ReadingExerciseApiResponse>(`api/Classes/Exercise?where=${query}`);
+  }
 
   create(title: string, sets: number, repetitions: string, programId: string, ownerId: string) {
     const exerciseData = {
